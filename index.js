d836ee7874acc6d73f65b16e3462130575debc7e
hesham931 <69353244+hesham931@users.noreply.github.com>
2020-12-21 23:00:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ function changeCoverForWard(){
         coverNumberForWard = 1;
 
     if(coverNumberForWard == 1){
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-1.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-1.jpg')";
         hText.innerHTML = "Discover innovative ways to decorate";
         pText.innerHTML = "We provide unmatched quality.Comfort,and style for property oweners across the country.Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.";
     }
     else if(coverNumberForWard == 2){
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-2.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-2.jpg')";
         hText.innerHTML = "We are available all across the globe";
         pText.innerHTML = "With stores all over the world,it`s easy for you to find furniture for our home or place of business.Locally,we`re in most major cities throuout the country.Find the branch nearest you using our store locator.Any question?Don`t hesitote to contact us today.";
     }
     else{
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-3.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-3.jpg')";
         hText.innerHTML = "Manufactured with the best materials";
         pText.innerHTML = "Our modren furniture store provide a high level of quality.Our company has invested in advanced technology to ensure that every product is made as perfect and as consists as possible.With three decades of experience in this industry,we understand what customers want for their home and office.";
     }
@@ -35,17 +35,17 @@ function changeCoverBackWard(){
         coverNumberForWard = 3;
 
     if(coverNumberForWard == 1){
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-1.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-1.jpg')";
         hText.innerHTML = "Discover innovative ways to decorate";
         pText.innerHTML = "We provide unmatched quality.Comfort,and style for property oweners across the country.Our experts combine form and function in bringing your vision to life.Create a room in your own style with our collection and make your property a reflection of you and what you love.";
     }
     else if(coverNumberForWard == 2){
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-2.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-2.jpg')";
         hText.innerHTML = "We are available all across the globe";
         pText.innerHTML = "With stores all over the world,it`s easy for you to find furniture for our home or place of business.Locally,we`re in most major cities throuout the country.Find the branch nearest you using our store locator.Any question?Don`t hesitote to contact us today.";
     }
     else{
-        backGroundId.backgroundImage = "url('project_images/desktop-image-hero-3.jpg')";
+        backGroundId.backgroundImage = "url('desktop-image-hero-3.jpg')";
         hText.innerHTML = "Manufactured with the best materials";
         pText.innerHTML = "Our modren furniture store provide a high level of quality.Our company has invested in advanced technology to ensure that every product is made as perfect and as consists as possible.With three decades of experience in this industry,we understand what customers want for their home and office.";
     }
